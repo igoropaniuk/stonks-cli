c1f58549ead4fb8bfd73a02d8e56d0aab138a143
Igor Opaniuk <igor.opaniuk@gmail.com>
2026-03-28 11:34:38 +0100

    refactor: move _TABLE_COLUMNS to neutral _columns.py module
    app.py was importing a private constant from show.py, creating a reverse
    dependency from the TUI to the CLI display layer. Move _TABLE_COLUMNS
    to a dedicated module and have both consumers import from there.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@gmail.com>

---
+++ src/stonks_cli/_columns.py
@@ -0,0 +1,12 @@
+"""Shared table-schema constants used by both the TUI and the CLI show command."""
+
+_TABLE_COLUMNS = (
+    "Instrument",
+    "Exchange",
+    "Qty",
+    "Avg Cost",
+    "Last Price",
+    "Daily Chg",
+    "Mkt Value",
+    "Unrealized P&L",
+)
